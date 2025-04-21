8099c7e7e3100bf0e22849f20cde285d925e1808
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2025-04-21 01:07:30 -0300

    fix

--- function_app/__init__.py
+++ function_app/__init__.py
@@ -4,22 +4,32 @@ import asyncio
 from playwright.async_api import async_playwright
 import os
 
+def encontrar_chromium_executavel():
+    path_base = os.path.join(os.getcwd(), ".playwright")
+    for nome in os.listdir(path_base):
+        if nome.startswith("chromium-"):
+            return os.path.join(path_base, nome, "chrome-linux", "chrome")
+    raise FileNotFoundError("Chromium não encontrado em .playwright")
+
 
 async def buscar_cref(cref: str):
-    playwright_path = os.path.join(os.getcwd(), ".playwright", "chromium-1112", "chrome-linux", "chrome")
+
+    executable_path = encontrar_chromium_executavel()
 
     async with async_playwright() as p:
+
         browser = await p.chromium.launch(
             headless=True,
-            executable_path=playwright_path
+            executable_path=executable_path
         )
+
         page = await browser.new_page()
 
         await page.goto(
             "https://sistemacref4.com.br/crefonline/SifaOnLineServicosPublicosAction.do?metodo=servicoPublicoProfissionais")
         await page.fill("#buscaRegistro", cref)
         await page.click("input[value='CONSULTAR']")
-        await page.wait_for_timeout(3000)
+        await page.wait_for_selector(".table-col-15", timeout=5000)
 
         try:
             cref_element = await page.query_selector(".table-col-15")
